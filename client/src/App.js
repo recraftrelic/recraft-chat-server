900caf8070b16a0f8f5dd88cb5493d7d810bccfc
MamtaAditiS <mamtasingla007@gmail.com>
2020-07-14 16:46:07 +0530

    fixed name-input and connection flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,72 @@
-import React, { useEffect } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import './App.css';
+
 const ENDPOINT = "http://127.0.0.1:4001"
 
-
 function App() {
+  const [ username, saveUsername ] = useState("")
+  const socket = socketIOClient(ENDPOINT);
+
+  const connectInChat = () => {
+    appendMessage('You Joined')
+    socket.emit('new-user', username)
+    socket.on('user-connected', (data) => {
+      appendMessage(`${username} connected in chat.`)
+    })
+  }
+
+  const appendMessage = message => {
+    const messageContainer = document.getElementById('message-container')
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message 
+    messageContainer.append(messageElement)
+  }
+
   useEffect(() => {
-      const socket = socketIOClient(ENDPOINT);
+    const messageForm = document.getElementById('send-container')
+    const messageInput = document.getElementById('message-input') 
 
-      const messageContainer = document.getElementById('message-container')
-      const messageForm = document.getElementById('send-container')
-      const messageInput = document.getElementById('message-input') 
-      
-      const name = prompt('What is your name?')
-      
-      appendMessage('You Joined')
-      socket.emit('new-user', name)
+    socket.on('user-disconnected', () => {
+      appendMessage(`${username} disconnected from chat.`)
+    })
 
-      socket.on('chat-message', data => {
-        appendMessage(`${data.name}: ${data.message}`)
-      })
+    socket.on('chat-message', data => {
+      console.log( data, "data")
+      appendMessage(`Other: ${data.message}`)
+    }) 
 
-      socket.on('user-connected', name => {
-        appendMessage(`${name} connected`)
-      })
+    messageForm.addEventListener('submit', e => {
+      e.preventDefault();
+      const message = messageInput.value
+      appendMessage(`You: ${message}`)
+      socket.emit('send-chat-message', message)
+      messageInput.value = ''
+    })
+  }, [])
 
-      socket.on('user-disconnected', name => {
-        appendMessage(`${name} disconnected`)
-      })
-
-      messageForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const message = messageInput.value
-        appendMessage(`You: ${message}`)
-        socket.emit('send-chat-message', message)
-        messageInput.value = ''
-      })
-
-      function appendMessage(message){
-        const messageElement = document.createElement('div')
-        messageElement.innerText = message
-        messageContainer.append(messageElement)
-      }
-  }, []);
+  
 
   return (
     <div className="container">
       <div className="wrapper">
-          <h1>Recraft Chat Server</h1>
-        <div id="message-container"/>
-        <form id="send-container">
-          <input type="text" id="message-input"/>
-          <button type="submit" id="send-button">Send</button>
-        </form>
-      </div> 
+        <h1>Recraft Chat Server</h1>
+          <div className="name-input-area">
+            <label className="label-text">Enter your name:</label>
+            <input type="text" id="name-input" value={username} onChange={(e) => saveUsername(e.target.value)}/>
+            <button type="submit" className="connect-button" onClick={connectInChat}>Connect</button>
+          </div> 
+          <Fragment>
+            <div id="message-container"/>
+            <form id="send-container">
+              <input type="text" id="message-input"/>
+              <button type="submit" id="send-button">Send</button>
+            </form>
+          </Fragment>      
+      </div>
     </div>
   )
 }
 
 export default App;
+
